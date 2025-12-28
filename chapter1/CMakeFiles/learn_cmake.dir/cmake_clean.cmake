0cc3ca6425a41e9e594b8f08fc6f7fdba0d95c29
zylyehuo <zyl_0606@163.com>
2025-12-28 19:05:07 +0800

    first commit

---
+++ chapter1/CMakeFiles/learn_cmake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learn_cmake.dir/hello.cpp.o"
+  "CMakeFiles/learn_cmake.dir/hello.cpp.o.d"
+  "learn_cmake"
+  "learn_cmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learn_cmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
