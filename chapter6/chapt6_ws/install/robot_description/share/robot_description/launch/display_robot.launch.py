f3a3fd41a880336078c01db081804fd2b2f76b40
zylyehuo <zyl_0606@163.com>
2026-01-05 16:22:45 +0800

    code update

---
+++ chapter6/chapt6_ws/install/robot_description/share/robot_description/launch/display_robot.launch.py
@@ -0,0 +1,44 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # 获取默认的 urdf 路径
+    urdf_package_path = get_package_share_directory("robot_description")  # 传入功能包的名字
+    urdf_file = os.path.join(urdf_package_path, "urdf", "robot.urdf")
+    
+    # 获取默认的 rviz 配置文件路径
+    rviz_config_file = os.path.join(urdf_package_path, "config", "display_robot_model.rviz")
+    
+    # 声明一个 urdf 路径的参数，方便修改
+    action_declare_urdf_file = launch.actions.DeclareLaunchArgument("urdf_file", default_value=urdf_file, description="加载的模型文件路径")
+    
+    # 通过文件路径获取内容，并转换成参数值对象，以供传入 robot_state_publisher
+    substitutions_command_result = launch.substitutions.Command(['cat ', launch.substitutions.LaunchConfiguration("urdf_file")])
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(substitutions_command_result, value_type=str)
+    
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description_value}],
+    )
+
+    action_joint_state_publisher = launch_ros.actions.Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+    )
+    
+    action_rviz_node = launch_ros.actions.Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", rviz_config_file],
+    )
+    
+    return launch.LaunchDescription([
+        action_declare_urdf_file,
+        action_robot_state_publisher,
+        action_joint_state_publisher,
+        action_rviz_node,
+    ])
+    
---
+++ chapter6/chapt6_ws/src/robot_description/urdf/urdf.md
@@ -0,0 +1,7 @@
+# 结构可视化
+cd /home/zylyehuo/ROS2_learning/chapter6/chapt6_ws/src/robot_description/urdf
+urdf_to_graphviz robot.urdf
+
+# 安装 urdf 状态发布节点
+sudo apt install ros-humble-joint-state-publisher
+sudo apt install ros-humble-robot-state-publisher
